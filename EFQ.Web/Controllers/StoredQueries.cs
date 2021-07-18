6df251c55b4fa33bd771f58e4172c6ff7ad5b469
Jeffrey Dege <jdege@jdege.us>
2021-07-18 12:45:35 -0500

    Beginning to add StoredQueries page

---
+++ EFQ.Web/Controllers/StoredQueries.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Linq;
+using System.Text;
+using JDege.EFQ.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JDege.EFQ.Web.Controllers
+{
+    public class StoredQueriesController : Controller
+    {
+        private ChinookDbContext dbContext;
+
+        public StoredQueriesController(ChinookDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
