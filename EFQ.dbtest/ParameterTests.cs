33e93acab4c7f6a6f4e73217093b4039bbeb313a
Jeffrey Dege <jdege@jdege.us>
2021-08-08 13:54:55 -0500

    More parameter tests

--- EFQ.dbtest/ParameterTests.cs
+++ EFQ.dbtest/ParameterTests.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Shouldly;
 using Xunit;
 using System.Collections.Generic;
+using System;
 
 namespace JDege.EFQ.dbtest
 {
@@ -19,19 +20,21 @@ namespace JDege.EFQ.dbtest
         }
 
         [Fact]
-        public async Task testContextParameterAsync()
+        public async Task testStringParameterAsync()
         {
+            var searchItemId = "Item 2";
+            var expectedName = "Another item";
             Seed(items: new[]
             {
                 new Item{itemId = "Item 1", name = "An item"},
-                new Item{itemId = "Item 2", name = "Another item"},
+                new Item{itemId = searchItemId, name = expectedName},
                 new Item{itemId = "Item 3", name = "A third item"},
             });
 
-            var sc = JDege.EFQ.EFQBuilder.Equal("itemId", "{{Context:anid}}");
+            var sc = JDege.EFQ.EFQBuilder.Equal("itemId", "{{Context:searchItemId}}");
 
             var context = new Dictionary<string, EFQ.Constant> {
-                {"anid", new EFQ.Constant("Item 2")}
+                {"searchItemId", new EFQ.Constant(searchItemId)}
             };
 
             using (var dbContext = new TestDbContext(ContextOptions))
@@ -39,9 +42,62 @@ namespace JDege.EFQ.dbtest
                 var predicate = sc.ConstructPredicate<Item>(context);
                 var results = await dbContext.Items.Where(predicate).ToListAsync();
                 results.Count.ShouldBe(1);
-                results[0].name.ShouldBe("Another item");
+                results[0].name.ShouldBe(expectedName);
             }
         }
 
+        [Fact]
+        public async Task testIntParameterAsync()
+        {
+            var searchAmount = 2;
+            var expectedItemId = "Item 2";
+            Seed(items: new[]
+            {
+                new Item{itemId = "Item 1", amount = 1},
+                new Item{itemId = expectedItemId, amount = searchAmount},
+                new Item{itemId = "Item 3", amount = 3},
+            });
+
+            var sc = JDege.EFQ.EFQBuilder.Equal("amount", "{{Context:searchAmount}}");
+
+            var context = new Dictionary<string, EFQ.Constant> {
+                {"searchAmount", new EFQ.Constant(searchAmount)}
+            };
+
+            using (var dbContext = new TestDbContext(ContextOptions))
+            {
+                var predicate = sc.ConstructPredicate<Item>(context);
+                var results = await dbContext.Items.Where(predicate).ToListAsync();
+                results.Count.ShouldBe(1);
+                results[0].itemId.ShouldBe(expectedItemId);
+            }
+        }
+
+        [Fact]
+        public async Task testDateParameterAsync()
+        {
+            var searchWhen = DateTime.Parse("2020-01-01");
+            var expectedItemId = "Item 2";
+            Seed(items: new[]
+            {
+                new Item{itemId = "Item 1", when= DateTime.Parse("2020-07-14")},
+                new Item{itemId = expectedItemId, when = searchWhen},
+                new Item{itemId = "Item 3", when = DateTime.Parse("2020-03-11")},
+            });
+
+            var sc = JDege.EFQ.EFQBuilder.Equal("when", "{{Context:searchWhen}}");
+
+            var context = new Dictionary<string, EFQ.Constant> {
+                {"searchWhen", new EFQ.Constant(searchWhen)}
+            };
+
+            using (var dbContext = new TestDbContext(ContextOptions))
+            {
+                var predicate = sc.ConstructPredicate<Item>(context);
+                var results = await dbContext.Items.Where(predicate).ToListAsync();
+                results.Count.ShouldBe(1);
+                results[0].itemId.ShouldBe(expectedItemId);
+            }
+        }
     }
 }
