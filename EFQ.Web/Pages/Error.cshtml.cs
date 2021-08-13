7dfe0dbcb6b939293a51849a780cead299dce475
Jeffrey Dege <jdege@jdege.us>
2021-08-13 17:15:52 -0500

    Removed unused .cshtml.cs files

--- EFQ.Web/Pages/Error.cshtml.cs
+++ EFQ.Web/Pages/Error.cshtml.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-// TODO: Do we need Index.cshtml.cs?
 namespace JDege.EFQ.Web.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -23,7 +22,6 @@ namespace JDege.EFQ.Web.Pages
 
         public void OnGet()
         {
-            // TODO: What are we doing with RequestId?
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
